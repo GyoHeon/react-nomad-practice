d02c1e835ca13f6bda407aed7cbf04facee3125a
GyoHeonLee <ghl12078@gmail.com>
2022-01-14 13:03:15 +0900

    course: 6.1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const onClick = () => setCounter(counter + 1);
+  const onClick = () => setCounter((counter) => counter + 1);
+  console.log("i run all time");
+
+  useEffect(() => console.log("i run only onetime"), []);
 
   return (
     <div>
       <h1>{counter}</h1>
-
       <button onClick={onClick}>Click me</button>
     </div>
   );
