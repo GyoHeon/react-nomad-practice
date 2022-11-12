26b641d2ed4ed3c9631792a695c05aa3153d7bd3
GyoHeon Lee <gh.lee@muzlive.com>
2022-11-12 16:59:17 +0900

    Course: 4.2

--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -21,7 +21,28 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="relative p-6 rounded-3xl bg-white -top-5">
+          <div className="flex justify-between items-end -top-16 relative">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$230</span>
+            </div>
+          </div>
+          <div className="flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-xl font-medium">Tony Molly</span>
+            <span className="text-sm text-gray-500">New York, USA</span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
