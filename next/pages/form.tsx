83c81d89064720ae558e07e1c400ca177262309c
GyoHeonLee <ghl12078@gmail.com>
2022-12-31 03:35:16 +0900

    Course: 7.4

--- next/pages/form.tsx
+++ next/pages/form.tsx
@@ -1,31 +1,40 @@
-import { useForm } from "react-hook-form";
+import { FieldErrors, useForm } from "react-hook-form";
+
+interface LoginForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 const Form = () => {
-  const { register, handleSubmit, watch } = useForm();
-  const onValid = () => {
+  const { register, handleSubmit } = useForm<LoginForm>({});
+  const onValid = (data: LoginForm) => {
     console.log("valid!");
+  };
+  const onInValid = (errors: FieldErrors) => {
+    console.log(errors);
   };
 
   return (
-    <form onSubmit={handleSubmit(onValid)}>
+    <form onSubmit={handleSubmit(onValid, onInValid)}>
       <input
         {...register("username", {
-          required: true,
+          required: "Username is required",
+          minLength: {
+            message: "The username is too short",
+            value: 5,
+          },
         })}
         type="text"
         placeholder="Username"
       />
       <input
-        {...register("email", {
-          required: true,
-        })}
+        {...register("email", { required: "Email is required" })}
         type="email"
         placeholder="Email"
       />
       <input
-        {...register("password", {
-          required: true,
-        })}
+        {...register("password", { required: "Password is required" })}
         type="password"
         placeholder="Password"
       />
