68144d6c448bb5a18bb34a684233f3e8b7c7b118
GyoHeonLee <ghl12078@gmail.com>
2023-01-07 13:13:21 +0900

    Course: 9.3

--- next/pages/api/users/enter.tsx
+++ next/pages/api/users/enter.tsx
@@ -1,35 +1,33 @@
 import client from "@libs/server/client";
-import withHandler from "@libs/server/withHandler";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
   const { email, phone } = req.body;
-  const type = phone ? { phone: +phone } : { email };
+  const user = phone ? { phone: +phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
   const payload = Math.floor(100000 + Math.random() * 900000) + "";
-  const user = await client.user.upsert({
-    where: {
-      ...type,
-    },
-    create: {
-      name: "Anonymous",
-      ...type,
-    },
-    update: {},
-  });
   const token = await client.token.create({
     data: {
       payload,
       user: {
-        connect: {
-          id: user.id,
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
         },
       },
     },
   });
 
-  console.log({ token });
-
-  return res.status(200).end();
+  return res.json({ ok: true });
 }
 
 export default withHandler("POST", handler);
