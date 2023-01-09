3afa9528d1cf5fb1e599ebfb73cf7d254c7c26eb
GyoHeonLee <ghl12078@gmail.com>
2023-01-10 01:16:16 +0900

    Course: 9.7

--- next/pages/api/users/confirm.ts
+++ next/pages/api/users/confirm.ts
@@ -1,14 +1,29 @@
+import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withIronSessionApiRoute } from "iron-session/next";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseType>
 ) {
-  const { token } = req.body;
-  console.log(token);
+  const token = await client.token.findUnique({
+    where: { payload: req.body.token },
+    include: { user: true },
+  });
+
+  if (!token) res.status(404).end;
+
+  req.session.user = {
+    id: token.id,
+  };
+
+  await req.session.save();
 
   return res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotMarket",
+  password: process.env.IRON_PASSWORD,
+});
