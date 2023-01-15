111dc04002d3168f26e81d0f26f8c9473e6a689a
GyoHeonLee <ghl12078@gmail.com>
2023-01-16 03:40:11 +0900

    Course: 10.3

--- next/libs/client/useUser.ts
+++ next/libs/client/useUser.ts
@@ -1,18 +1,11 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((response) => response.json());
 
 export default function useUser() {
+  const { data, error } = useSWR("/api/users/me", fetcher);
   const router = useRouter();
-  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then((res) => res.json())
-      .then((data) => {
-        if (!data.ok) return router.replace("/enter");
-
-        setUser(data.profile);
-      });
-  }, []);
-  return user;
+  return data;
 }
