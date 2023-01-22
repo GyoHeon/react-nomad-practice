151ee0b7fb9fdf0c7e8cee797dde6086f7adf448
GyoHeonLee <ghl12078@gmail.com>
2023-01-22 23:38:50 +0900

    Course: 11.4

---
+++ next/pages/api/products/[id].ts
@@ -0,0 +1,31 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { id } = req.query;
+  const product = await client.product.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+  console.log(product);
+  res.json({ ok: true, product });
+}
+
+export default withApiSession(
+  withHandler({ methods: ["GET", "POST"], handler, isPrivate: true })
+);
